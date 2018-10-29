0763401f7535a9c79083621b819d6b19aab0f566
xjx00 <xjxyklwx@126.com>
2018-10-29 20:39:13 +0800

    upload python

---
+++ 1709140227.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#-*-coding:utf-8-*-
+#author: 17光电02 徐建祥
+
+year = input("请输入年份:")
+if (year%4 == 0 and year%100 != 0) or year%400 == 0:
+    print("是闰年")
+else:
+    print("不是闰年")
+
